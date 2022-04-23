62cd8f1ce114928747a506722055c2d1476d9393
xjl <1730648029@qq.com>
2022-04-23 09:55:14 +0800

    第一次提交！

---
+++ index.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: your name
+ * @Date: 2022-04-23 09:39:49
+ * @LastEditTime: 2022-04-23 09:44:55
+ * @LastEditors: your name
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \git练习\index.js
+ */
+window.alert('hello')
